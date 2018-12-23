24900730f5a82bffc3b0e71f9e53b7c2f6cc302f
Your Git's Name <Your Email>
2018-12-23 14:29:44 +0700

    [FE] Add template layouts admin to project

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::group(array('namespace' => 'Admin', 'prefix' => 'admin'), function() {
+    Route::get('/', 'DashboardController@index');
+});
