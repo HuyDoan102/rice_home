c82ece0781fa1b23f24c72c0cf0a29ae8edc365a
Nguyen Hong Doan Huy <huy.nhd@workhouse.me>
2018-12-24 13:47:03 +0700

    [BE] Create seed for project

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +14,16 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'role_id' => 2,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => bcrypt('123123123'), // secret
+        'phone' => $faker->phoneNumber,
+        'avatar' => $faker->imageUrl($width = 640, $height = 480, 'people'),
+        'status' => $faker->numberBetween(1, 2),
         'remember_token' => str_random(10),
     ];
 });
